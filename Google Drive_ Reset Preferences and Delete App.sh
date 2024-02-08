b97cf178af2bc200dfbda49ff15395af3407abcf
itinyk <59558503+itinyk@users.noreply.github.com>
2024-02-08 15:01:28 +0000

    Google Drive Scripts

---
+++ Google Drive_ Reset Preferences and Delete App.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+loggedInUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')
+
+if [ "$loggedInUser" == "" ]; then
+    echo 'No logged-in user detected.'
+    exit 1
+else
+    echo "$loggedInUser"
+fi
+
+killall -2 "Google Drive"
+
+sleep 2
+
+rm -rf /Users/$loggedInUser/Library/Application\ Support/Google/DriveFS/
+rm -rf "/Applications/Google Drive.app"
+killall -2 Finder
---
+++ Google Drive_ Reset Preferences.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+loggedInUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')
+
+if [ "$loggedInUser" == "" ]; then
+    echo 'No logged-in user detected.'
+    exit 1
+else
+    echo "$loggedInUser"
+fi
+
+killall -2 "Google Drive"
+
+sleep 2
+
+rm -rf /Users/$loggedInUser/Library/Application\ Support/Google/DriveFS/
+
+killall -2 Finder
+
+sleep 2
+
+open -a "Google Drive"
