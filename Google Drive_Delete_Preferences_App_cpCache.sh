fdaf3bbbd65ec113673ccb9c29de3561e0daba31
iTinyK <59558503+itinyk@users.noreply.github.com>
2024-02-28 13:23:24 +0000

    Create Google Drive_Delete_Preferences_App_cpCache.sh

---
+++ Google Drive_Delete_Preferences_App_cpCache.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+loggedInUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')
+
+if [ "$loggedInUser" == "" ]; then
+    echo 'No logged-in user detected.'
+    exit 1
+else
+    echo "$loggedInUser"
+fi
+
+killall -9 "Google Drive"
+
+sleep 5
+
+rm -rf "/Users/$loggedInUser/Library/Application Support/Google/DriveFS"
+rm -rf "/Applications/Google Drive.app"
+
+sleep 5
+
+killall -9 Finder
+
+sleep 5
+
+cp -rf /Users/$loggedInUser/Library/CloudStorage/ /Users/Shared/
