7eb38fb1cfc94ff49f1098bee9dc47dcf4230f86
iTinyK <59558503+itinyk@users.noreply.github.com>
2024-04-08 19:31:10 +0100

    Create Google Drive_Delete_Preferences_App_mvCache_Reboot.sh

---
+++ Google Drive_Delete_Preferences_App_mvCache_Reboot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+loggedInUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')
+
+if [ "$loggedInUser" == "" ]; then
+    echo 'No logged-in user detected.'
+    exit 1
+else
+    echo "$loggedInUser"
+fi
+
+killall -9 "Google Drive"
+
+sleep 5
+
+rm -rf "/Users/$loggedInUser/Library/Application Support/Google/DriveFS"
+rm -rf "/Users/$loggedInUser/Library/Application Support/FileProvider/com.google.drivefs.fpext"
+rm -rf "/Applications/Google Drive.app"
+
+sleep 5
+
+killall -9 Finder
+
+sleep 5
+
+mv /Users/$loggedInUser/Library/CloudStorage/ /Users/Shared/CloudStorage/
+
+shutdown -r now
